9e8834c5d29f60dc3abe016332f40f175cf9d6ec
kirikira <kiri_so@outlook.com>
2017-11-25 21:13:27 +0800

    fixtls

--- writejson.py
+++ writejson.py
@@ -14,12 +14,12 @@ def Write():
     openjsonfile=file("/etc/v2ray/config.json","w+")
     openjsonfile.writelines(myjsondump)
     openjsonfile.close()
-    
+
 #更改UUID
 def WriteUUID(myuuid):
     config[u"inbound"][u"settings"][u"clients"][0][u"id"]=str(myuuid)
     Write()
-    
+
 #更改端口
 def WritePort(myport):
     config[u"inbound"][u"port"]=int(myport)
@@ -29,7 +29,7 @@ def WritePort(myport):
 def WriteSecurity(mysecurity):
     config[u"inbound"][u"settings"][u"clients"][0][u"security"]=str(mysecurity)
     Write()
-    
+
 #更改底层传输设置
 def WriteStreamNetwork(network,para):
     security_backup=config[u"inbound"][u"streamSettings"][u"security"]
@@ -44,7 +44,7 @@ def WriteStreamNetwork(network,para):
         http=json.load(streamfile)
         http[u"tcpSettings"][u"header"][u"request"][u"headers"][u"Host"]=para
         config[u"inbound"][u"streamSettings"]=http
-        
+
     if (network == "ws"):
         streamfile=file("/usr/local/v2ray.fun/json_template/ws.json")
         ws=json.load(streamfile)
@@ -55,23 +55,23 @@ def WriteStreamNetwork(network,para):
         streamfile=file("/usr/local/v2ray.fun/json_template/kcp.json")
         kcp=json.load(streamfile)
         config[u"inbound"][u"streamSettings"]=kcp
-        
+
     if (network == "mkcp" and para=="kcp utp"):
         streamfile=file("/usr/local/v2ray.fun/json_template/kcp_utp.json")
         utp=json.load(streamfile)
         config[u"inbound"][u"streamSettings"]=utp
-        
+
     if (network == "mkcp" and para=="kcp srtp"):
         streamfile=file("/usr/local/v2ray.fun/json_template/kcp_srtp.json")
         srtp=json.load(streamfile)
         config[u"inbound"][u"streamSettings"]=srtp
 
-        
+
     if (network == "mkcp" and para=="kcp wechat-video"):
         streamfile=file("/usr/local/v2ray.fun/json_template/kcp_wechat.json")
         wechat=json.load(streamfile)
         config[u"inbound"][u"streamSettings"]=wechat
-    
+
     config[u"inbound"][u"streamSettings"][u"security"] = security_backup
     config[u"inbound"][u"streamSettings"][u"tlsSettings"] = tls_settings_backup
     Write()
@@ -79,7 +79,7 @@ def WriteStreamNetwork(network,para):
 #更改TLS设置
 def WriteTLS(action,domain):
     if action == "on":
-        crt_file = "/root/.acme.sh/" + domain + "/fullchain.cer"
+        crt_file = "/root/.acme.sh/" + domain + "/" + domain + ".key"
         key_file = "/root/.acme.sh/" + domain + "/ca.cer"
         config[u"inbound"][u"streamSettings"][u"security"] = "tls"
         tls_file = file("/usr/local/v2ray.fun/json_template/tlssettings.json")
